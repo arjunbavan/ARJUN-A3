f638fba57545e4b96964f3ca272f1f2aef2bab36
arjunbavan <83655150+arjunbavan@users.noreply.github.com>
2023-01-12 09:26:37 +0530

    Add files via upload

---
+++ gp anagram.py
@@ -0,0 +1,8 @@
+a=str(input("enter the word: "))
+b=str(input("enter the word: "))
+c=sorted(a)
+d=sorted(b)
+if(c==d):
+    print("anagram")
+else:
+    print("not anangram")
---
+++ staircase.py
@@ -0,0 +1,5 @@
+n=int(input("Enter the number os stair: "))
+if(n<=3):
+    print(n)
+else:
+    print("the number of stairs to be used is: ",(n-1)+(n-2))
